e1de09a8b50491140b6375b3f49b21a4783b4e8f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-08 22:18:07 -0400

    // Boilerplate template

---
+++ script.js
@@ -0,0 +1,30 @@
+// Boilerplate template
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+})
+
+// Active Navigation
+window.addEventListener('scroll', function () {
+  let fromTop = window.scrollY
+
+  document.querySelectorAll('nav a').forEach(link => {
+    let section = document.querySelector(link.hash)
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add('active')
+    } else {
+      link.classList.remove('active')
+    }
+  })
+})
