36755be2b85d55838fd6d813636a53b629530d5f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-10 16:44:54 -0400

    major updates to finish landing page and creating directories for sub pages

--- script.js
+++ script.js
@@ -11,9 +11,29 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   })
 })
 
-// Active Navigation
+// Highlight active section in navigation
+
 window.addEventListener('scroll', function () {
   let fromTop = window.scrollY
+
+  // Check if at the bottom of the page
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    // At the bottom of the page, set the last link as active
+    document
+      .querySelectorAll('nav a')
+      .forEach(link => link.classList.remove('active'))
+    let lastLink =
+      document.querySelectorAll('nav a')[
+        document.querySelectorAll('nav a').length - 1
+      ]
+    if (lastLink.hash) {
+      lastLink.classList.add('active')
+    }
+    return
+  }
+
+  // Variable to keep track of the closest section
+  let closest = null
 
   document.querySelectorAll('nav a').forEach(link => {
     if (!link.hash) {
@@ -22,15 +42,25 @@ window.addEventListener('scroll', function () {
 
     let section = document.querySelector(link.hash)
 
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add('active')
-    } else {
-      link.classList.remove('active')
+    // Calculate distance from section to current scroll position
+    let distance = Math.abs(fromTop - section.offsetTop)
+
+    // Update closest if this section is closer
+    if (closest === null || Math.abs(fromTop - closest.offsetTop) > distance) {
+      closest = section
     }
   })
+
+  // Remove active class from all links
+  document.querySelectorAll('nav a').forEach(link => {
+    link.classList.remove('active')
+  })
+
+  // Set the closest section's corresponding link to active
+  if (closest) {
+    let link = document.querySelector(`nav a[href="#${closest.id}"]`)
+    link.classList.add('active')
+  }
 })
 
 //Scroll to Top
