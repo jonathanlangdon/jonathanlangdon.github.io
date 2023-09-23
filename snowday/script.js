a4fc8c18fd9e11b28ba0c450ecd35a6e5a8bc564
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-09-23 19:04:08 -0400

    css updated due to bootstrap

--- snowday/script.js
+++ snowday/script.js
@@ -1,3 +1,9 @@
+function errorGettingWeather() {
+  document
+    .getElementById('error-container')
+    .insertAdjacentText('beforeend', '|| Error fetching weather data ||')
+}
+
 function getWeather() {
   const latitude = document.querySelector('#latitude').value
   const longitude = document.querySelector('#longitude').value
@@ -9,6 +15,36 @@ function getWeather() {
     }
   })
     .then(response => response.json())
+    // Get forecast data
+    .then(data => {
+      const snowToday = data.properties.forecast
+      fetch(snowToday, {
+        headers: {
+          'User-Agent': 'calvaryeagles.org (langdon@calvaryeagles.org)'
+        }
+      })
+        .then(response => response.json())
+        // Get Snow inches tonight
+        .then(data => {
+          const forecastTonight = data.properties.periods.find(period => {
+            return period.name.includes('Tonight')
+          })
+          if (forecastTonight) {
+            const forecastDescription = forecastTonight.detailedForecast
+            const regex = /\b(\d+(\.\d+)?)\s*(inch|inches)\b/
+            const match = forecastDescription.match(regex)
+            if (match) {
+              const inches = parseFloat(match[1])
+              document.querySelector('snow-today').value = inches
+            } else {
+              document.querySelector('snow-today').value = 0
+            }
+          } else {
+            errorGettingWeather
+          }
+        })
+    })
+    // Get hourly forecast data
     .then(data => {
       const forecastHourlyUrl = data.properties.forecastHourly
       fetch(forecastHourlyUrl, {
@@ -17,6 +53,7 @@ function getWeather() {
         }
       })
         .then(response => response.json())
+        // Get 7am temp forecast
         .then(data => {
           const tomorrow7amForecast = data.properties.periods.find(period => {
             return period.startTime.includes('T07:00')
@@ -25,6 +62,7 @@ function getWeather() {
             document.getElementById('temp').value =
               tomorrow7amForecast.temperature
           } else {
+            document.getElementById('temp').value = ''
             document
               .getElementById('error-container')
               .insertAdjacentText(
@@ -32,6 +70,7 @@ function getWeather() {
                 '|| No forecast available for 7am tomorrow ||'
               )
           }
+          // Get 5am precipitation
           const tomorrow5amForecast = data.properties.periods.find(period => {
             return period.startTime.includes('T05:00')
           })
@@ -39,6 +78,7 @@ function getWeather() {
             document.getElementById('precip').value =
               tomorrow5amForecast.probabilityOfPrecipitation.value
           } else {
+            document.getElementById('precip').value = ''
             document
               .getElementById('error-container')
               .insertAdjacentText(
@@ -50,15 +90,16 @@ function getWeather() {
     })
     .catch(error => {
       console.error('Error fetching weather data:', error)
-      document
-        .getElementById('error-container')
-        .insertAdjacentText('beforeend', '|| Error fetching weather data ||')
+      document.getElementById('temp').value = ''
+      document.getElementById('precip').value = ''
+      errorGettingWeather
     })
 }
 
 document
   .querySelector('#get-forecast-form')
   .addEventListener('submit', function (event) {
+    document.querySelector('#error-container').innerHTML = ''
     event.preventDefault()
     getWeather()
   })
