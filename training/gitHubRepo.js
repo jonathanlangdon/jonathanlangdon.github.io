cab1a85df877142987540e66558987d6eb82fd95
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-05 09:43:14 -0500

    GitHub API added to dynamically count ChatGPT challenges

---
+++ training/gitHubRepo.js
@@ -0,0 +1,29 @@
+async function fetchGitHubRepos() {
+  try {
+    const response = await fetch(
+      'https://api.github.com/users/jonathanlangdon/repos'
+    )
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching GitHub repos:', error)
+    throw error
+  }
+}
+
+// Example usage:
+fetchGitHubRepos()
+  .then(repo => {
+    challengeCount = repo[0].size
+    document.getElementById(
+      'chatGPTRepoCount'
+    ).innerText = `Repository of ${challengeCount} challenges`
+  })
+  .catch(error => {
+    console.log('Error fetching GitHub data')
+  })
