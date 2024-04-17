3a68fb211054c0b3b08225e5be257c070e6b04b6
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-04-17 08:48:16 -0400

    github repo updated

--- training/gitHubRepo.js
+++ training/gitHubRepo.js
@@ -2,31 +2,30 @@ async function fetchGitHubRepos() {
   try {
     const response = await fetch(
       'https://api.github.com/users/jonathanlangdon/repos'
-    )
+    );
 
     if (!response.ok) {
-      throw new Error('Network response was not ok')
+      throw new Error('Network response was not ok');
     }
 
-    const data = await response.json()
-    return data
+    const data = await response.json();
+    return data;
   } catch (error) {
-    console.error('Error fetching GitHub repos:', error)
-    throw error
+    console.error('Error fetching GitHub repos:', error);
+    throw error;
   }
 }
 
-// Example usage:
 fetchGitHubRepos()
   .then(repo => {
-    challengeCount = repo[0].size
+    challengeCount = repo.find(r => r.name === 'chatGPT_challenges').size;
     document.getElementById(
       'chatGPTRepoCount'
-    ).innerText = `Repository of ${challengeCount} challenges`
+    ).innerText = `Repository of ${challengeCount} challenges`;
   })
   .catch(error => {
     document.getElementById(
       'chatGPTRepoCount'
-    ).innerText = `Repository of ChatGPT challenges`
-    console.log('Error fetching GitHub data')
-  })
+    ).innerText = `Repository of ChatGPT challenges`;
+    console.log('Error fetching GitHub data');
+  });
