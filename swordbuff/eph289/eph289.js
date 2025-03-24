c39cc18445c4bd10f1b6bee0cc807223976096a3
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-03-24 16:27:30 -0400

    combining ephesians 2:8-9

--- swordbuff/eph289/eph289.js
+++ swordbuff/eph289/eph289.js
@@ -3,23 +3,9 @@ export const data = {
     {
       book: 'Ephesians',
       chapter: 2,
-      text: 'For it is by grace you have been saved, through faith—',
-      verse_start: 8.1,
-      verse_end: 8.1
-    },
-    {
-      book: 'Ephesians',
-      chapter: 2,
-      text: 'and this is not from yourselves, it is the gift of God—',
-      verse_start: 8.2,
-      verse_end: 8.2
-    },
-    {
-      book: 'Ephesians',
-      chapter: 2,
-      text: 'not by works, so that no one can boast.',
-      verse_start: 9,
-      verse_end: 9
+      text: 'For it is by grace you have been saved, through faith —and this is not from yourselves, it is the gift of God —not by works, so that no one can boast.',
+      verse_start: 8.9,
+      verse_end: 8.9
     }
   ]
 };
