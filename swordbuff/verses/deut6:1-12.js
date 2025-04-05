824c59fd4a4239e1a5e73db17f5953e771be44a4
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-04-05 18:43:42 -0400

    deut 6 for Brielle

---
+++ swordbuff/verses/deut6:1-12.js
@@ -0,0 +1,97 @@
+export const data = {
+  book: 'Deuteronomy',
+  translation: 'NIV-2011',
+  who: 'Brielle',
+  verses: [
+    {
+      chapter: 6,
+      verse: '1a',
+      text: 'These are the commands, decrees and laws the Lord your God directed me to teach you'
+    },
+    {
+      chapter: 6,
+      verse: '1b',
+      text: 'to observe in the land that you are crossing the Jordan to possess,'
+    },
+    {
+      chapter: 6,
+      verse: '2a',
+      text: 'so that you, your children and their children after them may fear the Lord your God as long as you live'
+    },
+    {
+      chapter: 6,
+      verse: '2b',
+      text: 'by keeping all his decrees and commands that I give you, and so that you may enjoy long life.'
+    },
+    {
+      chapter: 6,
+      verse: '3a',
+      text: 'Hear, Israel, and be careful to obey so that it may go well with you'
+    },
+    {
+      chapter: 6,
+      verse: '3b',
+      text: 'and that you may increase greatly in a land flowing with milk and honey, just as the Lord, the God of your ancestors, promised you.'
+    },
+    {
+      chapter: 6,
+      verse: '4',
+      text: 'Hear, O Israel: The Lord our God, the Lord is one.'
+    },
+    {
+      chapter: 6,
+      verse: '5',
+      text: 'Love the Lord your God with all your heart and with all your soul and with all your strength.'
+    },
+    {
+      chapter: 6,
+      verse: '6',
+      text: 'These commandments that I give you today are to be on your hearts.'
+    },
+    {
+      chapter: 6,
+      verse: '7a',
+      text: 'Impress them on your children. Talk about them when you sit at home'
+    },
+    {
+      chapter: 6,
+      verse: '7b',
+      text: 'and when you walk along the road, when you lie down and when you get up.'
+    },
+    {
+      chapter: 6,
+      verse: '8',
+      text: 'Tie them as symbols on your hands and bind them on your foreheads.'
+    },
+    {
+      chapter: 6,
+      verse: '9',
+      text: 'Write them on the doorframes of your houses and on your gates.'
+    },
+    {
+      chapter: 6,
+      verse: '10a',
+      text: 'When the Lord your God brings you into the land he swore to your fathers, to Abraham, Isaac and Jacob,'
+    },
+    {
+      chapter: 6,
+      verse: '10b',
+      text: 'to give you—a land with large, flourishing cities you did not build,'
+    },
+    {
+      chapter: 6,
+      verse: '11a',
+      text: 'houses filled with all kinds of good things you did not provide, wells you did not dig,'
+    },
+    {
+      chapter: 6,
+      verse: '11b',
+      text: 'and vineyards and olive groves you did not plant—then when you eat and are satisfied,'
+    },
+    {
+      chapter: 6,
+      verse: '12',
+      text: 'be careful that you do not forget the Lord, who brought you out of Egypt, out of the land of slavery.'
+    }
+  ]
+};
