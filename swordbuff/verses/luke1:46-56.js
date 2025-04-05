3c30eb13004a71b437ea44f55d3618f50d996335
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-04-05 18:44:47 -0400

    luke 1 for Brielle

---
+++ swordbuff/verses/luke1:46-56.js
@@ -0,0 +1,67 @@
+export const data = {
+  book: 'Luke',
+  translation: 'NIV-2011',
+  who: 'Brielle',
+  verses: [
+    {
+      chapter: 1,
+      verse: '45',
+      text: 'Blessed is she who has believed that the Lord would fulfill his promises to her!'
+    },
+    {
+      chapter: 1,
+      verse: '46',
+      text: 'And Mary said: “My soul glorifies the Lord'
+    },
+    {
+      chapter: 1,
+      verse: '47',
+      text: 'and my spirit rejoices in God my Savior,'
+    },
+    {
+      chapter: 1,
+      verse: '48',
+      text: 'for he has been mindful of the humble state of his servant. From now on all generations will call me blessed,'
+    },
+    {
+      chapter: 1,
+      verse: '49',
+      text: 'for the Mighty One has done great things for me—holy is his name.'
+    },
+    {
+      chapter: 1,
+      verse: '50',
+      text: 'His mercy extends to those who fear him, from generation to generation.'
+    },
+    {
+      chapter: 1,
+      verse: '51',
+      text: 'He has performed mighty deeds with his arm; he has scattered those who are proud in their inmost thoughts.'
+    },
+    {
+      chapter: 1,
+      verse: '52',
+      text: 'He has brought down rulers from their thrones but has lifted up the humble.'
+    },
+    {
+      chapter: 1,
+      verse: '53',
+      text: 'He has filled the hungry with good things but has sent the rich away empty.'
+    },
+    {
+      chapter: 1,
+      verse: '54',
+      text: 'He has helped his servant Israel, remembering to be merciful'
+    },
+    {
+      chapter: 1,
+      verse: '55',
+      text: 'to Abraham and his descendants forever, just as he promised our ancestors.”'
+    },
+    {
+      chapter: 1,
+      verse: '56',
+      text: 'Mary stayed with Elizabeth for about three months and then returned home.'
+    }
+  ]
+};
