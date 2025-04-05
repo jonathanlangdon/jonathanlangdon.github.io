c32d2191605c30919409c63137a142022d55007e
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-04-05 18:46:33 -0400

    psalm 139 for Brielle

---
+++ swordbuff/verses/psalm139.js
@@ -0,0 +1,127 @@
+export const data = {
+  book: 'Psalms',
+  translation: 'NIV-2011',
+  who: 'Brielle',
+  verses: [
+    {
+      chapter: 139,
+      verse: '1',
+      text: 'You have searched me, Lord, and you know me.'
+    },
+    {
+      chapter: 139,
+      verse: '2',
+      text: 'You know when I sit and when I rise; you perceive my thoughts from afar.'
+    },
+    {
+      chapter: 139,
+      verse: '3',
+      text: 'You discern my going out and my lying down; you are familiar with all my ways.'
+    },
+    {
+      chapter: 139,
+      verse: '4',
+      text: 'Before a word is on my tongue you, Lord, know it completely.'
+    },
+    {
+      chapter: 139,
+      verse: '5',
+      text: 'You hem me in behind and before, and you lay your hand upon me.'
+    },
+    {
+      chapter: 139,
+      verse: '6',
+      text: 'Such knowledge is too wonderful for me, too lofty for me to attain.'
+    },
+    {
+      chapter: 139,
+      verse: '7',
+      text: 'Where can I go from your Spirit? Where can I flee from your presence?'
+    },
+    {
+      chapter: 139,
+      verse: '8',
+      text: 'If I go up to the heavens, you are there; if I make my bed in the depths, you are there.'
+    },
+    {
+      chapter: 139,
+      verse: '9',
+      text: 'If I rise on the wings of the dawn, if I settle on the far side of the sea,'
+    },
+    {
+      chapter: 139,
+      verse: '10',
+      text: 'even there your hand will guide me, your right hand will hold me fast.'
+    },
+    {
+      chapter: 139,
+      verse: '11',
+      text: 'If I say, “Surely the darkness will hide me and the light become night around me,”'
+    },
+    {
+      chapter: 139,
+      verse: '12',
+      text: 'even the darkness will not be dark to you; the night will shine like the day, for darkness is as light to you.'
+    },
+    {
+      chapter: 139,
+      verse: '13',
+      text: "For you created my inmost being; you knit me together in my mother's womb."
+    },
+    {
+      chapter: 139,
+      verse: '14',
+      text: 'I praise you because I am fearfully and wonderfully made; your works are wonderful, I know that full well.'
+    },
+    {
+      chapter: 139,
+      verse: '15',
+      text: 'My frame was not hidden from you when I was made in the secret place, when I was woven together in the depths of the earth.'
+    },
+    {
+      chapter: 139,
+      verse: '16',
+      text: 'Your eyes saw my unformed body; all the days ordained for me were written in your book before one of them came to be.'
+    },
+    {
+      chapter: 139,
+      verse: '17',
+      text: 'How precious to me are your thoughts, God! How vast is the sum of them!'
+    },
+    {
+      chapter: 139,
+      verse: '18',
+      text: 'Were I to count them, they would outnumber the grains of sand—when I awake, I am still with you.'
+    },
+    {
+      chapter: 139,
+      verse: '19',
+      text: 'If only you, God, would slay the wicked! Away from me, you who are bloodthirsty!'
+    },
+    {
+      chapter: 139,
+      verse: '20',
+      text: 'They speak of you with evil intent; your adversaries misuse your name.'
+    },
+    {
+      chapter: 139,
+      verse: '21',
+      text: 'Do I not hate those who hate you, Lord, and abhor those who are in rebellion against you?'
+    },
+    {
+      chapter: 139,
+      verse: '22',
+      text: 'I have nothing but hatred for them; I count them my enemies.'
+    },
+    {
+      chapter: 139,
+      verse: '23',
+      text: 'Search me, God, and know my heart; test me and know my anxious thoughts.'
+    },
+    {
+      chapter: 139,
+      verse: '24',
+      text: 'See if there is any offensive way in me, and lead me in the way everlasting.'
+    }
+  ]
+};
