923e929bbc2bd6674839d8cd61b26633bbd2ce03
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-04-14 18:58:15 -0400

    Rev 5 for Ev and Heb 12 for Daddy

---
+++ swordbuff/verses/heb12:1-14.js
@@ -0,0 +1,107 @@
+export const data = {
+  book: 'Hebrews',
+  translation: 'NIV-84',
+  who: 'Daddy',
+  verses: [
+    {
+      chapter: 12,
+      verse: '1a',
+      text: 'therefore since we are surrounded by such a great cloud of witnesses let us throw off everything that hinders'
+    },
+    {
+      chapter: 12,
+      verse: '1b',
+      text: 'and the sin that so easily entangles and let us run with perseverance the race marked out for us'
+    },
+    {
+      chapter: 12,
+      verse: '2a',
+      text: 'let us fix our eyes on Jesus the author and perfecter of our faith'
+    },
+    {
+      chapter: 12,
+      verse: '2b',
+      text: 'who for the joy set before him endured the cross scorning its shame and sat down at the right hand of the throne of God'
+    },
+    {
+      chapter: 12,
+      verse: '3',
+      text: 'consider him who endured such opposition from sinful men so that you will not grow weary and lose heart'
+    },
+    {
+      chapter: 12,
+      verse: '4',
+      text: 'in your struggle against sin you have not yet resisted to the point of shedding your blood'
+    },
+    {
+      chapter: 12,
+      verse: '5a',
+      text: 'and you have forgotten that word of encouragement that addresses you as sons'
+    },
+    {
+      chapter: 12,
+      verse: '5b',
+      text: "my son do not make light of the Lord's discipline and do not lose heart when he rebukes you"
+    },
+    {
+      chapter: 12,
+      verse: '6',
+      text: 'because the Lord disciplines those he loves and he punishes everyone he accepts as a son'
+    },
+    {
+      chapter: 12,
+      verse: '7',
+      text: 'endure hardship as discipline God is treating you as sons for what son is not disciplined by his father'
+    },
+    {
+      chapter: 12,
+      verse: '8',
+      text: 'if you are not disciplined and everyone undergoes discipline then you are illegitimate children and not true sons'
+    },
+    {
+      chapter: 12,
+      verse: '9a',
+      text: 'moreover we have all had human fathers who disciplined us and we respected them for it'
+    },
+    {
+      chapter: 12,
+      verse: '9b',
+      text: 'how much more should we submit to the Father of our spirits and live'
+    },
+    {
+      chapter: 12,
+      verse: '10a',
+      text: 'our fathers disciplined us for a little while as they thought best'
+    },
+    {
+      chapter: 12,
+      verse: '10b',
+      text: 'but God disciplines us for our good that we may share in his holiness'
+    },
+    {
+      chapter: 12,
+      verse: '11a',
+      text: 'no discipline seems pleasant at the time but painful later on however'
+    },
+    {
+      chapter: 12,
+      verse: '11b',
+      text: 'it produces a harvest of righteousness and peace for those who have been trained by it'
+    },
+    {
+      chapter: 12,
+      verse: '12',
+      text: 'therefore strengthen your feeble arms and weak knees'
+    },
+    {
+      chapter: 12,
+      verse: '13',
+      text: 'make level paths for your feet so that the lame may not be disabled but rather healed'
+    },
+    {
+      chapter: 12,
+      verse: '14',
+      text: 'make every effort to live in peace with all men and to be holy without holiness no one will see the Lord'
+    }
+  ]
+};
---
+++ swordbuff/verses/rev5:13.js
@@ -0,0 +1,17 @@
+export const data = {
+  book: 'Revelation',
+  translation: 'NIV-11',
+  who: 'Evelyn',
+  verses: [
+    {
+      chapter: 5,
+      verse: '13a',
+      text: 'Then I heard every creature in heaven and on earth and under the earth and on the sea, and all that is in them, saying:'
+    },
+    {
+      chapter: 5,
+      verse: '13b',
+      text: '“To him who sits on the throne and to the Lamb be praise and honor and glory and power, for ever and ever!”'
+    }
+  ]
+};
