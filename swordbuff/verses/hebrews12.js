7826b0964dac3b347bb9866957740724e8a0192d
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-04-18 21:29:00 -0400

    hebrews 12 fully

--- swordbuff/verses/hebrews12.js
+++ swordbuff/verses/hebrews12.js
@@ -102,6 +102,106 @@ export const data = {
       chapter: 12,
       verse: '14',
       text: 'make every effort to live in peace with all men and to be holy without holiness no one will see the Lord'
+    },
+    {
+      chapter: 12,
+      verse: '15',
+      text: 'see to it that no one misses the grace of God and that no bitter root grows up to cause trouble and defile many'
+    },
+    {
+      chapter: 12,
+      verse: '16',
+      text: 'see that no one is sexually immoral or is godless like Esau who for a single meal sold his inheritance rights as the oldest son'
+    },
+    {
+      chapter: 12,
+      verse: '17a',
+      text: 'afterward as you know when he wanted to inherit this blessing he was rejected'
+    },
+    {
+      chapter: 12,
+      verse: '17b',
+      text: 'he could bring about no change of mind though he sought the blessing with tears'
+    },
+    {
+      chapter: 12,
+      verse: '18',
+      text: 'you have not come to a mountain that can be touched and that is burning with fire to darkness gloom and storm'
+    },
+    {
+      chapter: 12,
+      verse: '19',
+      text: 'to a trumpet blast or to such a voice speaking words that those who heard it begged that no further word be spoken to them'
+    },
+    {
+      chapter: 12,
+      verse: '20',
+      text: 'because they could not bear what was commanded if even an animal touches the mountain it must be stoned'
+    },
+    {
+      chapter: 12,
+      verse: '21',
+      text: 'the sight was so terrifying that Moses said I am trembling with fear'
+    },
+    {
+      chapter: 12,
+      verse: '22a',
+      text: 'but you have come to Mount Zion to the heavenly Jerusalem the city of the living God'
+    },
+    {
+      chapter: 12,
+      verse: '22b',
+      text: 'you have come to thousands upon thousands of angels in joyful assembly'
+    },
+    {
+      chapter: 12,
+      verse: '23a',
+      text: 'to the church of the firstborn whose names are written in heaven'
+    },
+    {
+      chapter: 12,
+      verse: '23b',
+      text: 'you have come to God the judge of all men to the spirits of righteous men made perfect'
+    },
+    {
+      chapter: 12,
+      verse: '24',
+      text: 'to Jesus the mediator of a new covenant and to the sprinkled blood that speaks a better word than the blood of Abel'
+    },
+    {
+      chapter: 12,
+      verse: '25a',
+      text: 'see to it that you do not refuse him who speaks if they did not escape when they refused him who warned them on earth'
+    },
+    {
+      chapter: 12,
+      verse: '25b',
+      text: 'how much less will we if we turn away from him who warns us from heaven'
+    },
+    {
+      chapter: 12,
+      verse: '26',
+      text: 'at that time his voice shook the earth but now he has promised once more I will shake not only the earth but also the heavens'
+    },
+    {
+      chapter: 12,
+      verse: '27',
+      text: 'the words once more indicate the removing of what can be shaken that is created things so that what cannot be shaken may remain'
+    },
+    {
+      chapter: 12,
+      verse: '28',
+      text: 'therefore since we are receiving a kingdom that cannot be shaken'
+    },
+    {
+      chapter: 12,
+      verse: '28',
+      text: 'let us be thankful and so worship God acceptably with reverence and awe'
+    },
+    {
+      chapter: 12,
+      verse: '29',
+      text: 'for our God is a consuming fire'
     }
   ]
 };
