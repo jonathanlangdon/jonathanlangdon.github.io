bf00daf2a90ba5930d2113dca2738780eb6309ae
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-04-19 12:13:31 -0400

    fixed evelyn mark verse

---
+++ swordbuff/verses/mark10:14.js
@@ -0,0 +1,12 @@
+export const data = {
+  book: 'Mark',
+  translation: 'NIV',
+  who: 'Evelyn',
+  verses: [
+    {
+      chapter: 10,
+      text: 'Let the little children come to me and do not hinder them for the kingdom of God belongs to such as these',
+      verse: '14'
+    }
+  ]
+};
