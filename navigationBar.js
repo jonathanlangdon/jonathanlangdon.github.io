85f4fdb5a677316ddb98e5923684c1b70a8718df
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-05-19 22:44:48 -0400

    updates for graduation and other updates

--- navigationBar.js
+++ navigationBar.js
@@ -1,34 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navItems = [
     { text: 'Story', link: '/#mystory' },
+    { text: 'Projects', link: '/#projects' },
     { text: 'Training', link: '/#training' },
     { text: 'Experience', link: '/#languages' },
     { text: 'Tools', link: '/#tools' },
     { text: 'Goals', link: '/#goals' },
-    { text: 'Projects', link: '/#projects' },
     { text: 'Resume', link: '/#resume' }
-  ]
+  ];
 
   // Function to create a navigation item
   const createNavItem = navItem => {
-    const listItem = document.createElement('li')
-    listItem.className = 'nav-item'
+    const listItem = document.createElement('li');
+    listItem.className = 'nav-item';
 
-    const anchor = document.createElement('a')
-    anchor.className = 'nav-link'
-    anchor.href = navItem.link
-    anchor.innerText = navItem.text
+    const anchor = document.createElement('a');
+    anchor.className = 'nav-link';
+    anchor.href = navItem.link;
+    anchor.innerText = navItem.text;
 
-    listItem.appendChild(anchor)
+    listItem.appendChild(anchor);
 
-    return listItem
-  }
+    return listItem;
+  };
 
   // Append navigation items to the container
-  const navContainer = document.querySelector('.navbar-nav')
+  const navContainer = document.querySelector('.navbar-nav');
 
   navItems.forEach(navItem => {
-    const listItem = createNavItem(navItem)
-    navContainer.appendChild(listItem)
-  })
-})
+    const listItem = createNavItem(navItem);
+    navContainer.appendChild(listItem);
+  });
+});
