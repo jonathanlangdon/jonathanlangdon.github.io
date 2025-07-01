fdd1c0c7f4d4d2e42315c5b115237bd3f4bb0a6e
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-07-01 10:47:33 -0400

    new evelyn verse for 1 tim

---
+++ swordbuff/verses/1Timothy3:11.js
@@ -0,0 +1,12 @@
+export const data = {
+  book: '1 Timothy',
+  translation: 'NIV-11',
+  who: 'Evelyn',
+  verses: [
+    {
+      chapter: 3,
+      text: 'in the same way the women are to be worthy of respect not malicious talkers but temperate and trustworthy in everything',
+      verse: '11'
+    }
+  ]
+};
