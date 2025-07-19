e7ee2162d3724856c3a23eec8173d7bd942adcb7
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-07-18 23:31:17 -0400

    john 1 verse added

---
+++ swordbuff/verses/john1:1-18.js
@@ -0,0 +1,97 @@
+export const data = {
+  book: 'John',
+  translation: 'NIV-84',
+  who: 'Daddy',
+  verses: [
+    {
+      chapter: 1,
+      verse: '1',
+      text: '1 in the beginning was the Word and the Word was with God and the Word was God'
+    },
+    {
+      chapter: 1,
+      verse: '2',
+      text: '2 he was with God in the beginning'
+    },
+    {
+      chapter: 1,
+      verse: '3',
+      text: '3 through him all things were made without him nothing was made that has been made'
+    },
+    {
+      chapter: 1,
+      verse: '4',
+      text: '4 in him was life and that life was the light of men'
+    },
+    {
+      chapter: 1,
+      verse: '5',
+      text: '5 the light shines in the darkness but the darkness has not understood it'
+    },
+    {
+      chapter: 1,
+      verse: '6',
+      text: '6 there came a man who was sent from God his name was John'
+    },
+    {
+      chapter: 1,
+      verse: '7',
+      text: '7 he came as a witness to testify concerning that light so that through him all men might believe'
+    },
+    {
+      chapter: 1,
+      verse: '8',
+      text: '8 he himself was not the light he came only as a witness to the light'
+    },
+    {
+      chapter: 1,
+      verse: '9',
+      text: '9 the true light that gives light to every man was coming into the world'
+    },
+    {
+      chapter: 1,
+      verse: '10',
+      text: '10 he was in the world and though the world was made through him the world did not recognize him'
+    },
+    {
+      chapter: 1,
+      verse: '11',
+      text: '11 he came to that which was his own but his own did not receive him'
+    },
+    {
+      chapter: 1,
+      verse: '12',
+      text: '12 yet to all who received him to those who believed in his name he gave the right to become children of God'
+    },
+    {
+      chapter: 1,
+      verse: '13',
+      text: '13 children born not of natural descent nor of human decision or a husband’s will but born of God'
+    },
+    {
+      chapter: 1,
+      verse: '14',
+      text: '14 the Word became flesh and made his dwelling among us we have seen his glory the glory of the One and Only who came from the Father full of grace and truth'
+    },
+    {
+      chapter: 1,
+      verse: '15',
+      text: '15 John testifies concerning him he cries out saying “This was he of whom I said ‘He who comes after me has surpassed me because he was before me’”'
+    },
+    {
+      chapter: 1,
+      verse: '16',
+      text: '16 from the fullness of his grace we have all received one blessing after another'
+    },
+    {
+      chapter: 1,
+      verse: '17',
+      text: '17 for the law was given through Moses grace and truth came through Jesus Christ'
+    },
+    {
+      chapter: 1,
+      verse: '18',
+      text: '18 no one has ever seen God but God the One and Only who is at the Father’s side has made him known'
+    }
+  ]
+};
