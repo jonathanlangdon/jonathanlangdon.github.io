50eb065ab61b6ab2d34a0cbf4d4a21df1b42ef02
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-11-04 20:48:26 -0500

    romans 12 punctuation removed

--- swordbuff/verses/romans12.js
+++ swordbuff/verses/romans12.js
@@ -1,137 +1,142 @@
 export const data = {
   book: 'Romans',
-  translation: 'NIV',
-  who: 'User',
+  translation: 'NIV-84',
+  who: 'Daddy',
   verses: [
     {
       chapter: 12,
-      verse: '1',
-      text: "1 Therefore, I urge you, brothers, in view of God's mercy, to offer your bodies as living sacrifices, holy and pleasing to God this is your spiritual act of worship."
+      verse: '1a',
+      text: '1 therefore I urge you brothers in view of gods mercy to offer your bodies'
+    },
+    {
+      chapter: 12,
+      verse: '1b',
+      text: 'as living sacrifices holy and pleasing to God this is your spiritual act of worship'
     },
     {
       chapter: 12,
       verse: '2a',
-      text: '2 Do not conform any longer to the pattern of this world, but be transformed by the renewing of your mind.'
+      text: '2 do not conform any longer to the pattern of this world but be transformed by the renewing of your mind'
     },
     {
       chapter: 12,
       verse: '2b',
-      text: "Then you will be able to test and approve what God's will is--his good, pleasing and perfect will."
+      text: 'then you will be able to test and approve what gods will is his good pleasing and perfect will'
     },
     {
       chapter: 12,
       verse: '3a',
-      text: '3 For by the grace given me I say to every one of you: Do not think of yourself more highly than you ought,'
+      text: '3 for by the grace given me I say to every one of you do not think of yourself more highly than you ought'
     },
     {
       chapter: 12,
       verse: '3b',
-      text: 'but rather think of yourself with sober judgment, in accordance with the measure of faith God has given you.'
+      text: 'but rather think of yourself with sober judgment in accordance with the measure of faith God has given you'
     },
     {
       chapter: 12,
       verse: '4',
-      text: '4 Just as each of us has one body with many members, and these members do not all have the same function,'
+      text: '4 just as each of us has one body with many members and these members do not all have the same function'
     },
     {
       chapter: 12,
       verse: '5',
-      text: '5 so in Christ we who are many form one body, and each member belongs to all the others.'
+      text: '5 so in christ we who are many form one body and each member belongs to all the others'
     },
     {
       chapter: 12,
       verse: '6',
-      text: "6 We have different gifts, according to the grace given us. If a man's gift is prophesying, let him use it in proportion to his faith."
+      text: '6 we have different gifts according to the grace given us if a mans gift is prophesying let him use it in proportion to his faith'
     },
     {
       chapter: 12,
       verse: '7',
-      text: '7 If it is serving, let him serve; if it is teaching, let him teach;'
+      text: '7 if it is serving let him serve if it is teaching let him teach'
     },
     {
       chapter: 12,
       verse: '8a',
-      text: '8 if it is encouraging, let him encourage; if it is contributing to the needs of others, let him give generously;'
+      text: '8 if it is encouraging let him encourage if it is contributing to the needs of others let him give generously'
     },
     {
       chapter: 12,
       verse: '8b',
-      text: 'if it is leadership, let him govern diligently; if it is showing mercy, let him do it cheerfully.'
+      text: 'if it is leadership let him govern diligently if it is showing mercy let him do it cheerfully'
     },
     {
       chapter: 12,
       verse: '9',
-      text: '9 Love must be sincere. Hate what is evil; cling to what is good.'
+      text: '9 love must be sincere hate what is evil cling to what is good'
     },
     {
       chapter: 12,
       verse: '10',
-      text: '10 Be devoted to one another in brotherly love. Honor one another above yourselves.'
+      text: '10 be devoted to one another in brotherly love honor one another above yourselves'
     },
     {
       chapter: 12,
       verse: '11',
-      text: '11 Never be lacking in zeal, but keep your spiritual fervor, serving the Lord.'
+      text: '11 never be lacking in zeal but keep your spiritual fervor serving the Lord'
     },
     {
       chapter: 12,
       verse: '12',
-      text: '12 Be joyful in hope, patient in affliction, faithful in prayer.'
+      text: '12 be joyful in hope patient in affliction faithful in prayer'
     },
     {
       chapter: 12,
       verse: '13',
-      text: "13 Share with God's people who are in need. Practice hospitality."
+      text: '13 share with gods people who are in need practice hospitality'
     },
     {
       chapter: 12,
       verse: '14',
-      text: '14 Bless those who persecute you; bless and do not curse.'
+      text: '14 bless those who persecute you bless and do not curse'
     },
     {
       chapter: 12,
       verse: '15',
-      text: '15 Rejoice with those who rejoice; mourn with those who mourn.'
+      text: '15 rejoice with those who rejoice mourn with those who mourn'
     },
     {
       chapter: 12,
       verse: '16',
-      text: '16 Live in harmony with one another. Do not be proud, but be willing to associate with people of low position. Do not be conceited.'
+      text: '16 live in harmony with one another do not be proud but be willing to associate with people of low position do not be conceited'
     },
     {
       chapter: 12,
       verse: '17',
-      text: '17 Do not repay anyone evil for evil. Be careful to do what is right in the eyes of everybody.'
+      text: '17 do not repay anyone evil for evil be careful to do what is right in the eyes of everybody'
     },
     {
       chapter: 12,
       verse: '18',
-      text: '18 If it is possible, as far as it depends on you, live at peace with everyone.'
+      text: '18 if it is possible as far as it depends on you live at peace with everyone'
     },
     {
       chapter: 12,
       verse: '19a',
-      text: "19 Do not take revenge, my friends, but leave room for God's wrath, for it is written:"
+      text: '19 do not take revenge my friends but leave room for gods wrath for it is written'
     },
     {
       chapter: 12,
       verse: '19b',
-      text: '"It is mine to avenge; I will repay," says the Lord.'
+      text: 'it is mine to avenge I will repay says the Lord'
     },
     {
       chapter: 12,
       verse: '20a',
-      text: '20 On the contrary: "If your enemy is hungry, feed him; if he is thirsty, give him something to drink.'
+      text: '20 on the contrary if your enemy is hungry feed him if he is thirsty give him something to drink'
     },
     {
       chapter: 12,
       verse: '20b',
-      text: 'In doing this, you will heap burning coals on his head."'
+      text: 'in doing this you will heap burning coals on his head'
     },
     {
       chapter: 12,
       verse: '21',
-      text: '21 Do not be overcome by evil, but overcome evil with good.'
+      text: '21 do not be overcome by evil but overcome evil with good'
     }
   ]
 };
