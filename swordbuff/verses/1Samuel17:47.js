e4a6edeaaa4909b705a2584eefa8d53a0f668d44
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-11-11 18:32:48 -0500

    evelyn 1 samuel 17

---
+++ swordbuff/verses/1Samuel17:47.js
@@ -0,0 +1,12 @@
+export const data = {
+  book: '1 Samuel',
+  translation: 'NIV',
+  who: 'Evelyn',
+  verses: [
+    {
+      chapter: 17,
+      text: 'All those gathered here will know that it is not by sword or spear that the Lord saves; for the battle is the Lord’s, and he will give all of you into our hands.”',
+      verse: '47'
+    }
+  ]
+};
