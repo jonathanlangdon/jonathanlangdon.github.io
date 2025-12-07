f7baa29033c8ad1e61c1464d27e69f22c6711e80
Jonathan Langdon <langdon.jonathan@gmail.com>
2025-12-07 16:13:26 -0500

    psalm 1 added

---
+++ swordbuff/verses/psalm1.js
@@ -0,0 +1,47 @@
+export const data = {
+  book: 'Psalms',
+  translation: 'NIV-84',
+  who: 'Daddy',
+  verses: [
+    {
+      chapter: 1,
+      verse: '1a',
+      text: '1 blessed is the man who does not walk in the counsel of the wicked'
+    },
+    {
+      chapter: 1,
+      verse: '1b',
+      text: 'or stand in the way of sinners or sit in the seat of mockers'
+    },
+    {
+      chapter: 1,
+      verse: '2',
+      text: '2 but his delight is in the law of the Lord and on his law he meditates day and night'
+    },
+    {
+      chapter: 1,
+      verse: '3a',
+      text: '3 he is like a tree planted by streams of water which yields its fruit in season'
+    },
+    {
+      chapter: 1,
+      verse: '3b',
+      text: 'and whose leaf does not wither whatever he does prospers'
+    },
+    {
+      chapter: 1,
+      verse: '4',
+      text: '4 not so the wicked they are like chaff that the wind blows away'
+    },
+    {
+      chapter: 1,
+      verse: '5',
+      text: '5 therefore the wicked will not stand in the judgment nor sinners in the assembly of the righteous'
+    },
+    {
+      chapter: 1,
+      verse: '6',
+      text: '6 for the Lord watches over the way of the righteous but the way of the wicked will perish'
+    }
+  ]
+};
