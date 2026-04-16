2546b6d260e26f10c65c61a54d6b1009ca4cf206
Jonathan Langdon <langdon.jonathan@gmail.com>
2026-04-16 17:01:03 -0400

    psalm 46 added

---
+++ swordbuff/verses/psalm46.js
@@ -0,0 +1,62 @@
+export const data = {
+  book: 'Psalms',
+  translation: 'NIV-84',
+  who: 'Daddy',
+  verses: [
+    {
+      chapter: 46,
+      verse: '1',
+      text: 'God is our refuge and strength an ever-present help in trouble'
+    },
+    {
+      chapter: 46,
+      verse: '2',
+      text: 'therefore we will not fear though the earth give way and the mountains fall into the heart of the sea'
+    },
+    {
+      chapter: 46,
+      verse: '3',
+      text: 'though its waters roar and foam and the mountains quake with their surging'
+    },
+    {
+      chapter: 46,
+      verse: '4',
+      text: 'there is a river whose streams make glad the city of God the holy place where the Most High dwells'
+    },
+    {
+      chapter: 46,
+      verse: '5',
+      text: 'God is within her she will not fall God will help her at break of day'
+    },
+    {
+      chapter: 46,
+      verse: '6',
+      text: 'nations are in uproar kingdoms fall he lifts his voice the earth melts'
+    },
+    {
+      chapter: 46,
+      verse: '7',
+      text: 'the Lord Almighty is with us the God of Jacob is our fortress'
+    },
+    {
+      chapter: 46,
+      verse: '8',
+      text: 'come and see the works of the Lord the desolations he has brought on the earth'
+    },
+    {
+      chapter: 46,
+      verse: '9',
+      text: 'he makes wars cease to the ends of the earth he breaks the bow and shatters the spear he burns the shields with fire'
+    },
+    {
+      chapter: 46,
+      verse: '10',
+      text: 'be still and know that I am God I will be exalted among the nations I will be exalted in the earth'
+    },
+    {
+      chapter: 46,
+      verse: '11',
+      text: 'the Lord Almighty is with us the God of Jacob is our fortress'
+    }
+  ]
+};
